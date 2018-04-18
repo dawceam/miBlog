e1766310885ef32aabe2bad4e74bf69a90011bd4
Jose Francisco <dawceam@gmail.com>
2018-04-18 19:21:07 +0200

    20180418

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,7 +1,12 @@
 class EstaticasController < ApplicationController
+  
   def contacto
+    @id = params[:id]
+    @pagina = params[:pagina]
   end
 
   def nosotros
+    @mensaje = "Hola desde el controlador"
+    @usuarios = ["Pablo", "María", "Silvia", "Daniel"]
   end
 end
